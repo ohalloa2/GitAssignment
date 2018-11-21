ca4c46b055a5c4c20f443ea5702adf55973cb43f
Aoife O'Halloran <ohalloa2@tcd.ie>
2018-11-21 14:17:42 +0000

    Changes made , accessing information from user jennybc as her account has a lot of activity

--- GitApiRequest.R
+++ GitApiRequest.R
@@ -9,6 +9,7 @@ library(httr)
 oauth_endpoints("github")
 # Change based on what your application is
 
+
 # Change based on what you 
 myapp <- oauth_app(appname = "GitAssignment",
                    key = "03cc3622ad0b4d8da559",
@@ -34,11 +35,7 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 #Above code sourced from https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
-#*******Above assignment for Github Access*******
-
 #******************************************************************************************************************************#
-
-#*****Assignment for building social graphs below****
 
 #Interrogating the GitAPI 
 #**************************#
@@ -47,70 +44,61 @@ gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 #stored in a data frame called 'myData'.  I can access different parts of the data 
 #frame using the $ operator, shown below.
 
-#I will store numerous differet details about my GitHub profile in a data frame called 'myInfo'. 
-#I will access different parts of the data frame using the $ operator
+mydata=fromJSON("https://api.github.com/users/ohalloa2")
+mydata$email #null return 
+mydata$name #Null return 
+mydata$login
+mydata$id
+mydata$followers #Give the number of people who follow my profile
+mydata$following
 
 #Accessing information about my followers 
 myFollowers = fromJSON("https://api.github.com/users/ohalloa2/followers") #Specific link to find details about my followers 
 myFollowers$login #Gives details of the usernames of all users who follow me
-noFollowers = length(myFollowers$login) #Give the number of people who follow my profile
-noFollowers
-
-#Other types of information available to me in myFollowers data base 
 myFollowers$type
 myFollowers$starred_url
+myFollowers$url
 
 #Accessing information about the users I am following 
 usersImFollowing = fromJSON("https://api.github.com/users/ohalloa2/following") 
 usersImFollowing$login #Gives details of the usernames of all the people I follow 
-noImFollowing = length(usersImFollowing$login)
-noImFollowing
-
-usersImFollowing$type #Details of the types of people I follow
-#everyone I follow is of type user 
+usersImFollowing$type #Details of the types of people I follow , everyone I follow is of type user 
 usersImFollowing$followers_url #this will give me the urls for later when I'm finding other users information 
 #**sample output: "https://api.github.com/users/toconno5/followers"  - used later in code 
-usersImFollowing$site_admin #Gives true/False details of whether the people I am following are admins or not 
-#no one I am following is a site administrator 
+usersImFollowing$site_admin #Gives true/False details of whether the people I am following are admins or not, no one I am following is a site administrator 
 
 #Accessing repository specific information 
 repos=fromJSON("https://api.github.com/users/ohalloa2/repos") #Specific link to find details about my different repositories 
 repos$name #Details of the names of my public repositories
 repos$created_at #Gives details of the date the repositories were created 
-
 repos$private #outlines whether my repositories are private or not 
-#repos$size .. unsure of what this is currently returning
-
 repos$language #Displays the languages of the different repositories 
 myLanguages = repos$language
 aggregate(data.frame(count = myLanguages), list(value =myLanguages), length) #Data frame displaying count for each language 
 
+#Not sure what this is currently doing
 df_uniq = unique(myLanguages)
 length(df_uniq)
 
 lcaRepos <- fromJSON("https://api.github.com/repos/ohalloa2/CS3012_LCA/commits")
 lcaRepos$commit$message #The details I included describing each commit to LCA assignment repository 
-#noCommits=length(lcaRepos$commit)
-#noCommits
 
 #Can repeat the process above for other users to gether information about their profiles 
 #Do this by changing the user name in the link
-toconno5Data = fromJSON("https://api.github.com/users/toconno5") #Data frame which holds toconno5's information 
-toconno5Data$followers #Displays number of followers
-toconno5Data$public_repos #Displays number of public repositories 
+jennybcData = fromJSON("https://api.github.com/users/jennybc") #Data frame which holds toconno5's information 
+jennybcData$followers #Displays number of followers
+jennybcData$public_repos #Displays number of public repositories 
 
-toconno5 <- fromJSON("https://api.github.com/users/toconno5/following")
-toconno5$login #names of all users that are following toconno5
+jennybcFollowing= fromJSON("https://api.github.com/users/jennybc/following")
+jennybcFollowing$login #names of all users that are following jennybc
+jennybcFollowing$url
 
-toconno5Repos=fromJSON("https://api.github.com/users/toconno5/repos") 
-toconno5Repos$name #names of all toconno5's repositories 
-#toconno5Repos$language
-#no=length(toconno5Repos$language)
-#no
+
+jennybcRepos=fromJSON("https://api.github.com/users/jennybc/repos") 
+jennybcRepos$name #names of all toconno5's repositories 
 
 #Accessing information of starred items by phadej
 starred = fromJSON("https://api.github.com/users/octocat/starred")
 starred$name
 
 
-#test if works 
